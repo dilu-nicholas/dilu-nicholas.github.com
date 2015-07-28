de766ef16399e9010ee65db758bade12f3b73ed7
Dilruan Nicholas <dilu-nicholas@users.noreply.github.com>
2015-07-28 15:33:49 -0400

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -9,3 +9,11 @@
   $(function() {
     $( "#selectable" ).selectable();
   });
+
+$(function() {
+    $( "input[type=submit], a, button" )
+      .button()
+      .click(function( event ) {
+        event.preventDefault();
+      });
+  });
