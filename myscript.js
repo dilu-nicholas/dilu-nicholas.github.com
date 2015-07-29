a44d807111618f403df723b9528d42a3dbb639d9
Dilruan Nicholas <dilu-nicholas@users.noreply.github.com>
2015-07-29 02:30:58 -0400

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -20,7 +20,7 @@ $(function() {
     $( "input[type=submit], a, button" )
       .button()
       .click(function( event ) {
-        event.preventDefault();
+        event.defaultPrevented();
       });
   });
 
