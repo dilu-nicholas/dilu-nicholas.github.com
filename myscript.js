74cab019ad5d838219e784ee26be3950d89fe177
Dilruan Nicholas <dilu-nicholas@users.noreply.github.com>
2015-07-28 21:47:45 -0400

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -24,6 +24,7 @@ $(function() {
       });
   });
 
+
 $(function() {
     $( "#tabs" ).tabs();
   });
@@ -42,4 +43,24 @@ $(function() {
 
 $(function() {
     $( "#accordion" ).accordion();
+  });
+
+$(function() {
+    var state = true;
+    $( "#button" ).click(function() {
+      if ( state ) {
+        $( "#effect" ).animate({
+          backgroundColor: "#aa0000",
+          color: "#fff",
+          width: 500
+        }, 1000 );
+      } else {
+        $( "#effect" ).animate({
+          backgroundColor: "#fff",
+          color: "#000",
+          width: 240
+        }, 1000 );
+      }
+      state = !state;
+    });
   });
