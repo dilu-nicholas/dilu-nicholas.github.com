8b28d27c26af963cd02d3b1a12f588e97b4a3823
Dilruan Nicholas <dilu-nicholas@users.noreply.github.com>
2015-07-28 21:03:31 -0400

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -2,16 +2,26 @@ $(function() {
     $( "#selectable" ).selectable();
   });
 
+$(document).ready(function() {
+		$('.myMenu > li').bind('mouseover', openSubMenu);
+		$('.myMenu > li').bind('mouseout', closeSubMenu);
+		
+		function openSubMenu() {
+			$(this).find('ul').css('visibility', 'visible');	
+		};
+		
+		function closeSubMenu() {
+			$(this).find('ul').css('visibility', 'hidden');	
+		};
+				   
+	});
+
 $(function() {
     $( "input[type=submit], a, button" )
       .button()
       .click(function( event ) {
         event.preventDefault();
       });
-  });
-
-$(function() {
-    $( "#menu" ).menu();
   });
 
 $(function() {
