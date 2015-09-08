699b57fb7adc7c2bbf2fe8e77b8b9dd0dfed69f8
Dilruan Nicholas <dilu-nicholas@users.noreply.github.com>
2015-09-08 19:31:04 -0400

    Update dilu_myscripts.js

--- dilu_myscripts.js
+++ dilu_myscripts.js
@@ -3,13 +3,6 @@
 function reciprocal(x) { alert ("1/x");
 }
 
-function f_x_sqr() {
-	with(document.calc) {
-		x.value = x.value * x.value;
-	}
-	keynum = 0;
-}
-
 function c(val) {
  document.getElementById("d").value=val;
 }
